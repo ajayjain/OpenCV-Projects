65e5c7b0d7e09d84f8d2296fe8f43e61fe790c90
Ajay Jain <ajayjain318@gmail.com>
2014-06-07 20:27:02 -0700

    Finish add weighted camera

--- add_weighted_camera.py
+++ add_weighted_camera.py
@@ -1,22 +1,24 @@
 import cv2
 
 logo = cv2.imread('img/opencv_logo.png')
-im = cv2.imread('img/cards.png')
-# im = cv2.imread('img/cards.png')[0:rows, 0:cols]
-
 rows, cols, channels = logo.shape
-smallim = cv2.resize(im, (cols, rows))
-# smallim = cv2.resize(im, logo.shape[:2][::-1])
-# cam = cv2.VideoCapture
 
-print logo.shape
-print smallim.shape
-
-added = cv2.addWeighted(smallim, 0.8,	# alpha = 0.7
-						logo, 0.2,	# beta  = 0.3
-						0)			# gamma - scalar added to each sum
+cap = cv2.VideoCapture(0)
+ret, frame = cap.read()
+shape = frame.shape[:2][::-1]
+logo = cv2.resize(logo, shape)
 
 cv2.namedWindow('addWeighted')
-cv2.imshow('addWeighted', added)
-cv2.waitKey(0)
+
+while True:
+	added = cv2.addWeighted(frame, 0.8,	# alpha = 0.7
+							logo, 0.2,	# beta  = 0.3
+							0)			# gamma - scalar added to each sum
+
+	cv2.imshow('addWeighted', added)
+	ret, frame = cap.read()
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+	
+cap.release()
 cv2.destroyAllWindows()
